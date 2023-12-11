8edd93c61492746a724b1b0324b9d541a794e1e3
Codux <codux@codux.com>
2023-12-11 18:03:47 +0200

    Moving static test to be added from tasksArray useState

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,34 @@ import Classnames from 'classnames';
 import TaskItem from './TaskItem'
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [tasksArray, setTasksArray] = useState(["Buy Milk", "Go Run", "Make Dinner", "Bake Cake"]);
+    
+    function AddItem (){
+        console.log("Add Item")
+        // console.log(event.target?.addEventListener)
+    }
 
     return (
         <main>
             <h2 className={styles.title}>My ToDo List</h2>
             <div className={styles.tasks}>
                 <ul className={styles['ul--tasks--list']}>
-                    <li className={styles['list--item']}>
-                        <h3 className={styles['list--added-task']}>Task 1</h3>
-                        <button className={styles['delete--task-button']}>Delete</button>
-                    </li>
-                    <li className={styles['list--item']}>
-                        <h3 className={styles['list--added-task']}>Task 2</h3>
-                        <button className={styles['delete--task-button']}>Delete</button>
-                    </li>
-                    <li className={styles['list--item']}>
-                        <h3 className={styles['list--added-task']}>Task 3</h3>
-                        <button className={styles['delete--task-button']}>Delete</button>
-                    </li>
-                    <TaskItem />
+                     {
+                        tasksArray.map((task) =>
+                            <li className={styles['list--item']}>
+                                <h3 className={styles['list--added-task']}>{task}</h3>
+                                <button className={styles['delete--task-button']}>Delete</button>
+                            </li>
+                        )
+                    }
+
+                    {/* <TaskItem /> */}
                 </ul>
                 <button className={styles['clear--all-button']}>Clear All</button>
             </div>
             <footer className={styles['footer--task']}>
-                <input className={styles['footer--input']} placeholder="Type your task here"/>
-                <button className={Classnames(styles['footer--task'], styles['footer--add-button'])}>Add</button>
+                <input type="text" className={styles['footer--input']} placeholder="Type your task here"/>
+                <button className={Classnames(styles['footer--task'], styles['footer--add-button'])} onClick={AddItem}>Add</button>
             </footer>
         </main>
     );
