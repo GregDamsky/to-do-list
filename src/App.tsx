45a478782e357a6bd1d15f8c46165c73ccaf248b
GregDamsky <gregdamsky@gmail.com>
2023-12-12 13:26:11 +0200

    Adding delete icon source to delete button

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Classnames from 'classnames';
 import TaskItem from './TaskItem'
 
 function App() {
-    const [tasksArray, setTasksArray] = useState([{id: 0, item: "Buy Milk"}, {id: 1, item: "Go Run"}, {id: 2, item: "Make Dinner"}, {id: 3, item: "Bake Cake"}]);
+    const [tasksArray, setTasksArray] = useState([{ id: 0, item: "Buy Milk" }, { id: 1, item: "Go Run" }, { id: 2, item: "Make Dinner" }, { id: 3, item: "Bake Cake" }]);
     const [newTaskItem, setNewTaskItem] = useState('')
-    
-    function TextTyping (event: ChangeEvent<HTMLInputElement>) {
+
+    function TextTyping(event: ChangeEvent<HTMLInputElement>) {
         event.preventDefault();
         setNewTaskItem(event.target.value)
     }
@@ -15,7 +15,7 @@ function App() {
 
     function AddItem() {
         console.log(newTaskItem)
-        setTasksArray([...tasksArray, {id: tasksArray.length, item: newTaskItem}])
+        setTasksArray([...tasksArray, { id: tasksArray.length, item: newTaskItem }])
         setNewTaskItem("")
     }
 
@@ -25,7 +25,7 @@ function App() {
         //setTasksArray(newTasksArray)
     }
 
-    function ClearTasksList () {
+    function ClearTasksList() {
         console.log("Emptying tasks list")
         setTasksArray([])
     }
@@ -39,7 +39,8 @@ function App() {
                         tasksArray.map((task) =>
                             <li className={styles['list--item']} key={task.id}>
                                 <h3 className={styles['list--added-task']}>{task.item}</h3>
-                                <button className={styles['delete--task-button']} onClick={() => DeleteTask(task)}>Delete</button>
+                                <button className={styles['delete--task-button']} onClick={() => DeleteTask(task)}>
+                                    <img src="../src/icons/delete.png" className={styles['delete--task-button']} height="20px" alt="" />Delete</button>
                             </li>
                         )
                     }
@@ -49,7 +50,7 @@ function App() {
                 <button className={styles['clear--all-button']} onClick={ClearTasksList}>Clear All</button>
             </div>
             <footer className={styles['footer--task']}>
-                <input type="text" className={styles['footer--input']} onChange={TextTyping} placeholder="Type your task here" value={newTaskItem}/>
+                <input type="text" className={styles['footer--input']} onChange={TextTyping} placeholder="Type your task here" value={newTaskItem} />
                 <button type="button" className={Classnames(styles['footer--task'], styles['footer--add-button'])} onClick={AddItem}>Add</button>
             </footer>
         </main>
