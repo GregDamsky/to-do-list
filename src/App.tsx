a628ddf918f07fce7bcc4d98ccbacef5d9e4a9e4
GregDamsky <gregdamsky@gmail.com>
2023-12-12 17:33:57 +0200

    Add Tasks component and add it to be in the App elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { ChangeEvent, useState } from 'react';
 import styles from './App.module.scss';
 import Classnames from 'classnames';
 import TaskItem from './TaskItem'
+import Tasks from './Tasks';
+import { Item } from './types';
+
+
 
 function App() {
     const [tasksArray, setTasksArray] = useState([{ id: 0, item: "Buy Milk" }, { id: 1, item: "Go Run" }, { id: 2, item: "Make Dinner" }, { id: 3, item: "Bake Cake" }]);
@@ -12,17 +16,15 @@ function App() {
         setNewTaskItem(event.target.value)
     }
 
-
     function AddItem() {
         console.log(newTaskItem)
         setTasksArray([...tasksArray, { id: tasksArray.length, item: newTaskItem }])
         setNewTaskItem("")
     }
 
-    function DeleteTask(task) {
-        const newTasksArray = tasksArray.filter((item) => { item.id != task.id })
-        console.log(newTasksArray)
-        //setTasksArray(newTasksArray)
+    function DeleteTask(id: number) {
+        const newTasksArray = tasksArray.filter((item) =>  item.id !== id) 
+        setTasksArray(newTasksArray)
     }
 
     function ClearTasksList() {
@@ -31,30 +33,17 @@ function App() {
     }
 
     return (
-        <main>
-            <h2 className={styles.title}>My ToDo List</h2>
-            <div className={styles.tasks}>
-                <ul className={styles['ul--tasks--list']}>
-                    {
-                        tasksArray.map((task) =>
-                            <li className={styles['list--item']} key={task.id}>
-                                <h3 className={styles['list--added-task']}>{task.item}</h3>
-                                <button className={styles['delete--task-button']} onClick={() => DeleteTask(task)}>
-                                    <img src="../src/icons/delete.png" className={styles['delete--task-button']} height="20px" alt="" />Delete</button>
-                            </li>
-                        )
-                    }
-
-                    {/* <TaskItem /> */}
-                </ul>
-                <button className={styles['clear--all-button']} onClick={ClearTasksList}>Clear All</button>
-            </div>
-            <footer className={styles['footer--task']}>
-                <input type="text" className={styles['footer--input']} onChange={TextTyping} placeholder="Type your task here" value={newTaskItem} />
-                <button type="button" className={Classnames(styles['footer--task'], styles['footer--add-button'])} onClick={AddItem}>Add</button>
-            </footer>
+         <main>
+             <h2 className={styles.title}>My ToDo List</h2>
+             <Tasks items={tasksArray} onDetele={DeleteTask} clearAll={ClearTasksList} />
+            
+             <footer className={styles['footer--task']}>
+                 <input type="text" className={styles['footer--input']} onChange={TextTyping} placeholder="Type your task here" value={newTaskItem} />
+                 <button type="button" className={Classnames(styles['footer--task'], styles['footer--add-button'])} onClick={AddItem}>Add</button>
+             </footer>
         </main>
     );
 }
 
 export default App;
+
---
+++ src/Tasks.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './App.module.scss';
+import { Item } from './types';
+
+
+function Tasks( props: {items: Item[], onDetele: (id: number) => void, clearAll: () => void}) {
+    
+
+    return (
+        <div className={styles.tasks}>
+        <ul className={styles['ul--tasks--list']}>
+            {
+                props.items.map((task) =>
+                    <li className={styles['list--item']} key={task.id}>
+                        <h3 className={styles['list--added-task']}>{task.item}</h3>
+                        <button className={styles['delete--task-button']} onClick={() => props.onDetele(task.id)}>
+                            <img src="../src/icons/delete.png" className={styles['delete--task-button']} height="20px" alt="" />Delete</button>
+                    </li>
+                )
+            }
+
+           {/* <TaskItem /> */}
+        </ul>
+        <button className={styles['clear--all-button']} onClick={props.clearAll}>Clear All</button>
+    </div>
+    );
+}
+
+export default Tasks
