0096444a9e46e22964bb73f929834bae71ab781f
GregDamsky <gregdamsky@gmail.com>
2023-12-14 10:48:48 +0200

    changing function names to be camelCase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { ChangeEvent, useState } from 'react';
 import styles from './App.module.scss';
 import TasksList from './components/TasksList';
-import { Item } from './types';
 import NewTask from './components/NewTask';
 
 
@@ -10,31 +9,29 @@ function App() {
     const [tasksArray, setTasksArray] = useState([{ id: 0, item: "Buy Milk" }, { id: 1, item: "Go Run" }, { id: 2, item: "Make Dinner" }, { id: 3, item: "Bake Cake" }]);
     const [newTaskItem, setNewTaskItem] = useState('')
 
-    function TextTyping(event: ChangeEvent<HTMLInputElement>) {
-        event.preventDefault();
+    function textTyping(event: ChangeEvent<HTMLInputElement>) {
         setNewTaskItem(event.target.value)
     }
 
-    function AddItem() {
+    function addItem() {
         setTasksArray([...tasksArray, { id: tasksArray.slice(-1)[0].id + 1, item: newTaskItem }])
         setNewTaskItem("")
     }
 
-    function DeleteTask(id: number) {
+    function deleteTask(id: number) {
         const newTasksArray = tasksArray.filter((item) =>  item.id !== id) 
         setTasksArray(newTasksArray)
     }
 
-    function ClearTasksList() {
+    function clearTasksList() {
         setTasksArray([])
     }
 
     return (
          <main>
              <h2 className={styles.title}>My ToDo List</h2>
-             <TasksList items={tasksArray} onDetele={DeleteTask} clearAll={ClearTasksList} />
-
-             <NewTask item={newTaskItem} typeText={TextTyping} onAdd={AddItem}/>
+             <TasksList items={tasksArray} onDetele={deleteTask} clearAll={clearTasksList} />
+             <NewTask item={newTaskItem}  typeText={textTyping} onAdd={addItem}/>
             
         </main>
     );
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,16 +1,12 @@
-import React, { ChangeEvent } from 'react';
+import { ChangeEvent } from 'react';
 import styles from '../App.module.scss';
-import { Item } from '../types';
 import Classnames from 'classnames';
 
 
-
 function NewTask( props: {item: string, onAdd: () => void, typeText: (event: ChangeEvent<HTMLInputElement>) => void}) {
-    
     return (
-
         <footer className={styles['footer--task']}>
-            <input type="text" className={styles['footer--input']} onChange={props.typeText} placeholder="Type your task here" />
+            <input type="text" className={styles['footer--input']} onChange={props.typeText} placeholder="Type your task here" value={props.item}/>
             <button type="button" className={Classnames(styles['footer--task'], styles['footer--add-button'])} onClick={props.onAdd}>Add</button>
         </footer>
 
