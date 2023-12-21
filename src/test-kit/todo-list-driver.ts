296eb4339b04f57917135246641d8f30ca3ae3bc
GregDamsky <gregdamsky@gmail.com>
2023-12-21 18:12:01 +0200

    [Feature]Add more functions to todo-lit-driver

--- src/test-kit/todo-list-driver.ts
+++ src/test-kit/todo-list-driver.ts
@@ -11,7 +11,7 @@ export class ToDoList{
     async getAllListItems() {
         return await this.page.getByRole('listitem').all();
     }
-      
+    
     async getDataByElementWithinListItems(listItems: Promise<Locator[]>, element?: string) {
         switch(element !== '') { 
             case (element === 'heading'): { 
@@ -34,9 +34,23 @@ export class ToDoList{
          }
     }
 
-    async getRandomIndex (){
-        const maxIndex = (await this.getAllListItems()).length
-        return Math.floor(Math.random() * maxIndex)
+    async getNumberOfItemsInList()
+    {
+        let maxIndex = (await this.getAllListItems()).length
+        if (maxIndex >= 0) return maxIndex
+        else {
+            console.log("Given list is empty")
+            return maxIndex = -1
+        } 
+    }
+
+    async getRandomIndex (maxIndex: number){
+        
+        if (maxIndex > -1) { 
+            return Math.floor(Math.random() * maxIndex)
+        }
+        console.log(`Wrong value range for random index ${maxIndex}`)
+        return maxIndex
         
     }
 
@@ -53,21 +67,45 @@ export class ToDoList{
     }
 
     async isTaskInList (task: string | null | undefined){
+        let isFound = false
+        let taskIndex = -1;
         if ( task !== undefined && task !== null) {
             console.log(`searching ${task} in the list...`)
+            taskIndex = 0
             for (const row of await this.getDataByElementWithinListItems(this.getAllListItems(), 'heading'))
             {
                 if (task === await (await row).textContent()) {
                     console.log (`task: ${task} was found in the list...`)
-                    return true
+                    isFound = true
+                    return {isFound, taskIndex}
                 }
+                taskIndex += 1; 
             }
+            taskIndex = -1
             console.log (`task: ${task} is not in the list...`)
-            return false
+            return {isFound, taskIndex}
         }
         else {
             console.log (`task: ${task} is not of type string`)
-            return false
+            return {isFound, taskIndex}
+        }
+    }
+
+    async deleteTaskFromList (task?: string | null | undefined) {
+        if (task === "" || task === null || task === undefined) {
+            console.log ("No task is selected... Thus we'll be randomizing a task to delete")
+            const taskIndexToDelete = await this.getRandomIndex(await this.getNumberOfItemsInList())
+            task = await this.getTaskFromList(taskIndexToDelete)
+        }
+        const {isFound, taskIndex} = await this.isTaskInList(task)
+        if (isFound) {
+            console.log(`Deleteing task: ${task}`)
+            const allDeleteButtons = await this.getDataByElementWithinListItems(this.getAllListItems(), 'button')
+            await (await allDeleteButtons[taskIndex]).getByText("Delete").click()
+            console.log(`Task: ${task} successfully deleted`)
+        }
+        else {
+            console.log(`Couldn't find task: ${task} in list... No tasks to delete`)
         }
     }
 
