036c0ead2d87665eec3b6052eb4d7f11e65708d9
GregDamsky <gregdamsky@gmail.com>
2023-12-26 14:57:28 +0200

    Addressing all Peter's comments

--- src/test-kit/todo-list-driver.ts
+++ src/test-kit/todo-list-driver.ts
@@ -9,12 +9,11 @@ export class ToDoList {
     }
 
     async getAllListItemTitles() {
-        return this.page.locator('.list--added-task').allTextContents();
+        return this.page.locator('[data-task-title]').allTextContents();
     }
 
     async getTaskLocatorByTitle(title: string) {
-        const taskIndex = await this.getTaskIndexFromList(title);
-        return this.page.getByRole('listitem').nth(taskIndex);
+        return this.page.locator(`[data-task="${title}"]`);
     }
 
     async getLocatorsByRole(listItems: Locator[], role: 'heading' | 'button') {
@@ -26,86 +25,49 @@ export class ToDoList {
     }
 
     async getTaskFromListByIndex(taskIndex: number) {
-        const tasks = await this.getLocatorsByRole(await this.getAllListItems(), 'heading');
-        if (taskIndex <= tasks.length)
-            return this.page
-                .getByRole('listitem')
-                .getByRole('heading')
-                .nth(taskIndex)
-                .textContent();
-        else {
-            console.log(`No such id (${taskIndex}) in ToDoList`);
-            return '';
+        const taskTitle = await this.page
+            .getByRole('listitem')
+            .getByRole('heading')
+            .nth(taskIndex)
+            .textContent();
+
+        if (!taskTitle) {
+            throw new Error(`No such id (${taskIndex}) in ToDoList`);
         }
+
+        return taskTitle;
     }
 
     async getTaskIndexFromList(task: string) {
         const titles = await this.getAllListItemTitles();
-        return titles.indexOf(task) ? titles.indexOf(task) : -1;
-    }
-
-    async isTaskListEqualsArrayItems(arr: string[]) {
-        let isIdentical = true;
-        if ((await this.getNumberOfItemsInList()) !== arr.length) {
-            isIdentical = false;
-            console.log(
-                "Amount of items in array isn't identical to amount of items in the task list"
-            );
-        }
-        for (const item of arr) {
-            if (!(await this.isTaskInList(item))) {
-                isIdentical = false;
-                console.log(`Item "${item}" isn't part of the task list`);
-                break;
-            }
-        }
-        return isIdentical;
+        return titles.indexOf(task);
     }
 
     async isTaskInList(task: string) {
-        // console.log(`searching ${task} in the list...`);
-        // const titles = await this.getAllListItemTitles();
-        // const isFound = titles.includes(task);
-        // isFound
-        //     ? console.log(`task: ${task} was found in the list...`)
-        //     : console.log(`task: ${task} is not in the list...`);
-        // return isFound;
+        console.log(`searching ${task} in the list...`);
+        const titles = await this.getAllListItemTitles();
+        const isFound = titles.includes(task);
+        isFound
+            ? console.log(`task: ${task} was found in the list...`)
+            : console.log(`task: ${task} is not in the list...`);
+        return isFound;
+    }
 
-        let isFound = false;
-        if (task !== undefined && task !== null) {
-            for (const row of await this.getLocatorsByRole(
-                await this.getAllListItems(),
-                'heading'
-            )) {
-                if (task === (await row.textContent())) {
-                    console.log(`task: ${task} was found in the list...`);
-                    isFound = true;
-                    return isFound;
-                }
-            }
-            console.log(`task: ${task} is not in the list...`);
-            return isFound;
-        } else {
-            console.log(`task: ${task} is not of type string`);
-            return isFound;
+    async deleteTaskFromList(task: string) {
+        if (!(await this.isTaskInList(task))) {
+            throw new Error(`Couldn't find task: "${task}" in list... No tasks to delete`);
         }
+
+        const mytask = await this.getTaskLocatorByTitle(task);
+        console.log(`deleting task: ${task}`);
+        const deleteButton = mytask.locator('button', { hasText: 'Delete' });
+        await deleteButton.click();
+        console.log(`Task: ${task} successfully deleted`);
     }
 
-    async deleteTaskFromList(task?: string) {
-        if (task === undefined) task = '';
-        const isFound = await this.isTaskInList(task);
-        if (isFound) {
-            const mytask = await this.getTaskLocatorByTitle(task);
-            console.log(`deleting task: ${task}`);
-            const deleteButton = mytask.locator('button');
-            await deleteButton.click();
-            console.log(`Task: ${task} successfully deleted`);
-        } else console.log(`Couldn't find task: "${task}" in list... No tasks to delete`);
-    }
-
-    async addTask(task?: string) {
+    async addTask(task: string) {
         console.log(`Adding a new task "${task}" to the list`);
-        await this.page.getByPlaceholder('Type your task here').fill(`${task}`);
+        await this.page.getByPlaceholder('Type your task here').fill(task);
     }
 
     async generateTask() {
--- src/test/tasks.test.ts
+++ src/test/tasks.test.ts
@@ -28,43 +28,27 @@ describe('My ToDo List Page Tests', () => {
         await closePage(context, browser);
     });
 
-    it('has all items in the list', async () => {
+    it('has all existing items in the list', async () => {
         const myToDoList = new ToDoList(page);
-        const myFullArrayOfTasks = ['Bake Cake', 'Buy Milk', 'Make Dinner', 'Go Run'];
-        const myPartialArrayOfTasks = ['Bake Cake', 'Go Run'];
-        const myArrayOfPartiallyDiffTasks = ['Bake Cake', 'Sell Milk', 'Eat Dinner', 'Go Run'];
-        expect(await myToDoList.isTaskListEqualsArrayItems(myPartialArrayOfTasks)).toBe(false);
-        expect(await myToDoList.isTaskListEqualsArrayItems(myArrayOfPartiallyDiffTasks)).toBe(
-            false
-        );
-        expect(await myToDoList.isTaskListEqualsArrayItems(myFullArrayOfTasks)).toBe(true);
+        const myFullArrayOfTasks = ['Buy Milk', 'Go Run', 'Make Dinner', 'Bake Cake'];
+        expect(await myToDoList.getAllListItemTitles()).toStrictEqual(myFullArrayOfTasks);
     });
 
-    it('deletes predefined task from the list', async () => {
+    it('deletes task from the list', async () => {
         const taskToRemove = 'Bake Cake';
-
         const myToDoList = new ToDoList(page);
         const itemsInListBeforeDeletion = await myToDoList.getNumberOfItemsInList();
-        const isFound1 = await myToDoList.isTaskInList(taskToRemove);
-        if (isFound1) {
-            await myToDoList.deleteTaskFromList(taskToRemove);
-
-            // expect(getAllItemsTitles).to.not.include(taskToRemove);
-
-            //run same test to check if task indeed deleted from the list
-            const isFound2 = await myToDoList.isTaskInList(taskToRemove);
-            expect(
-                isFound2,
-                `task ${taskToRemove} was not expected in the list, although it still exists`
-            ).toBe(false);
-
-            const itemsInListAfterDeletion = await myToDoList.getNumberOfItemsInList();
-            //number of list items after deletion should decrease by one
-            expect(itemsInListAfterDeletion).toEqual(itemsInListBeforeDeletion - 1);
-        } else {
+        if (!(await myToDoList.isTaskInList(taskToRemove))) {
             console.log(`task ${taskToRemove} not found in the list, nothing to remove!`);
             test.fail();
         }
+        await myToDoList.deleteTaskFromList(taskToRemove);
+        expect(
+            await myToDoList.isTaskInList(taskToRemove),
+            `task ${taskToRemove} was not expected in the list, although it still exists`
+        ).toBe(false);
+        const itemsInListAfterDeletion = await myToDoList.getNumberOfItemsInList();
+        expect(itemsInListAfterDeletion).toEqual(itemsInListBeforeDeletion - 1);
     });
 
     it('deletes all tasks in the list using the "clear all" button', async () => {
