a7f4a87a22bb73f1c73155b69c4264bcf0870bec
GregDamsky <gregdamsky@gmail.com>
2024-01-04 11:17:02 +0200

    1. add performActionOnTaskFromList function to generise all button actions of task list section 2. add buttonActions enum to contain all available buttons text 3. move myToDoList object alloaction to beforeEach section 4. add a new test for editing existing task

--- src/test/tasks.test.ts
+++ src/test/tasks.test.ts
@@ -3,10 +3,11 @@ import { Browser, BrowserContext, Page } from 'playwright';
 import { ToDoList } from '../test-kit/todo-list-driver';
 import { test, expect } from 'playwright/test';
 import { after } from 'mocha';
-import { getRandomIndex } from '../test-kit/helpers';
+import { buttonActions } from '../test-kit/helpers';
 
 describe('My ToDo List Page Tests', () => {
     let page: Page, context: BrowserContext, browser: Browser;
+    let myToDoList: ToDoList;
     const myInitialArrayOfTasks = ['Buy Milk', 'Go Run', 'Make Dinner', 'Bake Cake'];
 
     before(async function () {
@@ -18,6 +19,7 @@ describe('My ToDo List Page Tests', () => {
 
     beforeEach(async () => {
         page = await context.newPage();
+        myToDoList = new ToDoList(page);
         await page.goto('localhost:3000');
     });
 
@@ -30,48 +32,48 @@ describe('My ToDo List Page Tests', () => {
     });
 
     it('has all existing items in the list', async () => {
-        const myToDoList = new ToDoList(page);
         expect(await myToDoList.getAllListItemTitles()).toStrictEqual(myInitialArrayOfTasks);
     });
 
     it('deletes task from the list', async () => {
         const taskToRemove = 'Bake Cake';
-        const myToDoList = new ToDoList(page);
         const itemsInListBeforeDeletion = await myToDoList.getNumberOfItemsInList();
         if (!(await myToDoList.isTaskInList(taskToRemove))) {
             console.log(`task ${taskToRemove} not found in the list, nothing to remove!`);
             test.fail();
         }
-        await myToDoList.deleteTaskFromList(taskToRemove);
-        expect(
-            await myToDoList.isTaskInList(taskToRemove),
-            `task ${taskToRemove} was not expected in the list, although it still exists`
-        ).toBe(false);
+        await myToDoList.performActionOnTaskFromList(buttonActions.delete, taskToRemove);
+        expect(await myToDoList.isTaskInList(taskToRemove), `task ${taskToRemove} was not expected in the list, although it still exists`).toBe(false);
         const itemsInListAfterDeletion = await myToDoList.getNumberOfItemsInList();
         expect(itemsInListAfterDeletion).toEqual(itemsInListBeforeDeletion - 1);
     });
 
     it('deletes all tasks in the list using the "clear all" button', async () => {
-        const myToDoList = new ToDoList(page);
-        await myToDoList.clickOnButton('Clear All');
+        await myToDoList.clickOnButton(buttonActions.clearAll);
         expect(await myToDoList.getNumberOfItemsInList(), '').toBe(0);
     });
 
     it('adds a predefined task to the list', async () => {
         const taskToAdd = 'Travel to a new country';
-
-        const myToDoList = new ToDoList(page);
         await myToDoList.addTask(taskToAdd);
-        await myToDoList.clickOnButton('Add');
+        await myToDoList.clickOnButton(buttonActions.add);
         const isFound = await myToDoList.isTaskInList(taskToAdd);
         expect(isFound).toBe(true);
     });
 
     it('adds a randomly generated task to the list', async () => {
-        const myToDoList = new ToDoList(page);
         const task = await myToDoList.generateTask();
-        await myToDoList.clickOnButton('Add');
+        await myToDoList.clickOnButton(buttonActions.add);
         const isFound = await myToDoList.isTaskInList(task);
         expect(isFound).toBe(true);
+    });
+
+    it('edits task in the list', async () => {
+        const taskToEdit = 'Make Dinner';
+        const taskAfterEdit = 'Make My Favorite Breakfast';
+        await myToDoList.performActionOnTaskFromList(buttonActions.edit, taskToEdit);
+        await myToDoList.performActionOnTaskFromList(buttonActions.save, taskToEdit, taskAfterEdit);
+        const editedArrayOfTasks = myInitialArrayOfTasks.map((x) => x.replace(taskToEdit, taskAfterEdit));
+        expect(await myToDoList.getAllListItemTitles()).toStrictEqual(editedArrayOfTasks);
     });
 });
